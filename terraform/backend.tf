c21ebe6e83c41754e2d4fa56215fda4f82fe9575
Sudheesh Balakrishna <Sudheesh.balakrishna@stratogent.com>
2026-04-20 13:07:51 +0530

    updated the region in backend file

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "terraform-sudheesh"
     key =   "terraform-sudheesh/ThreeTier_DevOps_Pipeline/terraform.tfstate"
-    region = us-east-1
+    region = "us-east-1"
     use_lockfile = true
   }
 }
