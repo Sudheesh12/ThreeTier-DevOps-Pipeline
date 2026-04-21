571621db6d2d8f8fd943a6041760d7a7e7e03b8f
Sudheesh Balakrishna <Sudheesh.balakrishna@stratogent.com>
2026-04-21 12:51:10 +0530

    update the the workflow to run different workflow for plan and appy

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 module "ec2" {
-  source               = "./modules/ec2_instance"
-  ami_id               = var.ami_id
-  volume_size          = var.volume_size
-  instance_name        = var.instance_name
-  instance_type        = var.instance_type
-  allow_sg_cidr        = var.allow_sg_cidr
-  tags                 = var.tags
+  source        = "./modules/ec2_instance"
+  ami_id        = var.ami_id
+  volume_size   = var.volume_size
+  instance_name = var.instance_name
+  instance_type = var.instance_type
+  allow_sg_cidr = var.allow_sg_cidr
+  tags          = var.tags
 }
